4b6431ac5bcf9675c7ca71d18ecf3706b45c1247
Loren.B <lorenzo.bregant@alumnos.ispi4038.edu.ar>
2024-09-01 00:13:55 -0300

    Se agregaron más tests junto a la implementación del código

--- src/lista_de_tareas.py
+++ src/lista_de_tareas.py
@@ -3,7 +3,23 @@ class Tarea:
         self.__detalle = []
 
     def agregar_tarea(self, item):
-        self.__detalle.append(item)
+        self.__agregar_al_detalle(item)
 
     def cantidad_tareas(self):
+        return self.__cantidad_de_tareas()
+
+    def completar_tarea(self, item):
+        return self.__marcar_como_completada(item)
+
+    def __agregar_al_detalle(self, item):
+        self.__detalle.append({"titulo": item, "completada": False})
+
+    def __cantidad_de_tareas(self):
         return len(self.__detalle)
+
+    def __marcar_como_completada(self, item):
+        for tarea in self.__detalle:
+            if tarea["titulo"] == item:
+                tarea["completada"] = True
+                return True
+        return False
--- test/test_lista_de_tareas.py
+++ test/test_lista_de_tareas.py
@@ -8,3 +8,26 @@ def test_se_puede_agregar_una_tarea_a_la_lista():
     tarea.agregar_tarea('1')
 
     assert tarea.cantidad_tareas() == 1
+
+def test_se_pueden_agregar_multiples_tareas_a_la_lista():
+    tarea = Tarea()
+    tarea.agregar_tarea('1')
+    tarea.agregar_tarea('2')
+
+    assert tarea.cantidad_tareas() == 2
+
+def test_se_puede_completar_una_tarea():
+    tarea = Tarea()
+    tarea.agregar_tarea('comprar pan')
+
+    assert tarea.completar_tarea('comprar pan') is True
+
+def test_se_pueden_completar_varias_tareas():
+    tarea = Tarea()
+    tarea.agregar_tarea('comprar leche')
+    tarea.agregar_tarea('comprar azucar')
+    tarea.agregar_tarea('comprar yerba')
+
+    assert tarea.completar_tarea('comprar leche') is True
+    assert tarea.completar_tarea('comprar azucar') is True
+    assert tarea.completar_tarea('comprar yerba') is True
